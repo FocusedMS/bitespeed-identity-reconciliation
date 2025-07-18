bcf4ee9e8ca32572f7a3b2b7c5a0b3c58dd4306e
Madhu <madhusudhanchittibabu@gmail.com>
2025-07-18 13:04:43 +0530

    Initial project setup

---
+++ identity-reconciliation/src/main/java/com/bitespeed/identityreconciliation/IdentityReconciliationApplication.java
@@ -0,0 +1,11 @@
+package com.bitespeed.identityreconciliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityReconciliationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityReconciliationApplication.class, args);
+    }
+} 
